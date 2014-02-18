8d8065fbe323be74ec3fb422af89bc1a3bd3f4d7
eguitarz <dalema22@gmail.com>
2014-02-18 19:28:39 +0800

    add sinatra

--- main.rb
+++ main.rb
@@ -5,5 +5,4 @@ client = EvernoteOAuth::Client.new(
   consumer_secret: '5056cd90e0a9ce97',
   sandbox: true
  )
-request_token = client.request_token(:oauth_callback => 'http://localhost/')
-request_token.authorize_url
+request_token = client.request_token(:oauth_callback => 'http://localhost:4000/')
