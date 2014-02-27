51ec9a533231570e48f909a902b41d25761cacf6
eguitarz <dalema22@gmail.com>
2014-02-27 21:56:43 +0800

    refine

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require 'evernote_oauth'
+require "#{File.dirname(__FILE__)}/evernote"
+
 DEVELOPER_TOKEN = ENV['EVERNOTE_TOKEN']
 
 client = EvernoteOAuth::Client.new(
@@ -11,39 +13,6 @@ note_store = client.note_store
 notebooks = note_store.listNotebooks
 notebooks.each do |notebook|
   puts "Notebook: #{notebook.name}";
-end
-
-def make_note(note_store, note_title, note_body, parent_notebook=nil)
- 
-  n_body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-  n_body += "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">"
-  n_body += "<en-note>#{note_body}</en-note>"
- 
-  ## Create note object
-  our_note = Evernote::EDAM::Type::Note.new
-  our_note.title = note_title
-  our_note.content = n_body
- 
-  ## parent_notebook is optional; if omitted, default notebook is used
-  if parent_notebook && parent_notebook.guid
-    our_note.notebookGuid = parent_notebook.guid
-  end
- 
-  ## Attempt to create note in Evernote account
-  begin
-    note = note_store.createNote(our_note)
-  rescue Evernote::EDAM::Error::EDAMUserException => edue
-    ## Something was wrong with the note data
-    ## See EDAMErrorCode enumeration for error code explanation
-    ## http://dev.evernote.com/documentation/reference/Errors.html#Enum_EDAMErrorCode
-    puts "EDAMUserException: #{edue}"
-  rescue Evernote::EDAM::Error::EDAMNotFoundException => ednfe
-    ## Parent Notebook GUID doesn't correspond to an actual notebook
-    puts "EDAMNotFoundException: Invalid parent notebook GUID"
-  end
- 
-  ## Return created note object
-  note
 end
 
 make_note(note_store, 'Evernoty', '<h1>Test</h1>')
